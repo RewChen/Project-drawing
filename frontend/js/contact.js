e08f373ffb17dbe77133ffeff009cd299ac3603b
tttinett <tinerockza@gmail.com>
2025-10-16 23:36:02 +0700

    Delete frontend directory

--- frontend/js/contact.js
+++
@@ -1,8 +0,0 @@
-// ทำให้เมนูที่คลิกอยู่ active
-const navLinks = document.querySelectorAll('.navbar a');
-navLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    navLinks.forEach(l => l.classList.remove('active'));
-    link.classList.add('active');
-  });
-});
